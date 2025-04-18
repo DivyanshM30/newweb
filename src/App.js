041cf7fe1b4b348950d04659338c949dc2314e8c
Divyansh Mishra <devdivyansh17063010@gmail.com>
2025-04-18 21:24:41 +0530

    fix-again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Hero from './Hero';
 import StudentLogin from './student_login';
 
